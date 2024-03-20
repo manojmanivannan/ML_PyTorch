3b9be1b5ac84f9af6342893865d6d7a612075a4b
Manoj Manivannan <manojm18@live.in>
2024-03-20 11:42:47 +0000

    Update notebook and model

---
+++ prediction/auxFunction.py
@@ -0,0 +1,107 @@
+import numpy as np
+import pandas as pd
+
+# Function to create windowed sequences
+def create_windows_feature_target(data_array, window_size, stride=1):
+    """
+    Returns a numpy array of windowed data with X as the features and y as the target
+    Parameter:
+        data_array: 2D numpy array or pandas dataframe
+        window_size: int
+        stride: int
+    Return:
+        X: 2D numpy array
+        y: 1D numpy array
+    """
+
+    # check if data_array is an numpy array 
+    if isinstance(data_array, np.ndarray):
+        pass
+  
+    # check if data_array is a pandas dataframe
+    elif isinstance(data_array, pd.DataFrame):
+        data_array = data_array.to_numpy()
+        
+    else:
+        raise ValueError("data_array must be a numpy array or pandas dataframe.")
+    
+    X, y = [], []
+    for i in range(0, len(data_array) - window_size, stride):
+        X.append(data_array[i:i + window_size])
+        y.append(data_array[i + window_size])
+    return np.array(X), np.array(y)
+    
+def get_mean_std_from_array(data_array):
+    """
+    A function to calculate the mean and standard deviation along axis 0 (vertical) of a numpy array or pandas dataframe.
+    
+    Parameters:
+    data_array (numpy.ndarray or pandas.DataFrame): The input data to calculate mean and standard deviation from.
+    
+    Returns:
+    tuple: A tuple containing the mean and standard deviation along axis 0 of the input data_array.
+    """
+
+
+    # check if data_array is an numpy array 
+    if isinstance(data_array, np.ndarray):
+        pass
+  
+    # check if data_array is a pandas dataframe
+    elif isinstance(data_array, pd.DataFrame):
+        data_array = data_array.to_numpy()
+        
+    else:
+        raise ValueError("data_array must be a numpy array or pandas dataframe.")
+    
+    return np.mean(data_array, axis=0), np.std(data_array, axis=0)
+
+def normalize_mean_std(data_array, mean=None, std=None):
+    """
+    Normalize the data_array using the given mean and standard deviation.
+
+    Args:
+        data_array (numpy.ndarray or pd.DataFrame): The input data to be normalized.
+        mean (float): The mean value to be used for normalization.
+        std (float): The standard deviation value to be used for normalization.
+
+    Returns:
+        numpy.ndarray: The normalized data_array.
+    """
+
+
+    # check if data_array is an numpy array 
+    if isinstance(data_array, np.ndarray):
+        pass
+  
+    # check if data_array is a pandas dataframe
+    elif isinstance(data_array, pd.DataFrame):
+        data_array = data_array.to_numpy()
+        
+    else:
+        raise ValueError("data_array must be a numpy array or pandas dataframe.")
+    
+    if mean is None or std is None:
+        mean, std = get_mean_std_from_array(data_array)
+    
+    return (data_array - mean) / std
+
+def train_test_split(features, target, split_ratio=0.8):
+    """
+    A function to split the data into train and test sets based on the given split ratio.
+
+    Parameters:
+    train_data (numpy.ndarray or pandas.DataFrame): The training data to be split.
+    test_data (numpy.ndarray or pandas.DataFrame): The test data to be split.
+    split_ratio (float, optional): The ratio of the data to be used for training. Default is 0.8.
+
+    Returns:
+    tuple: A tuple containing the training features, training target, validation features, validation target.
+    """
+    assert len(features) == len(target)
+
+    split_index = int(len(features) * split_ratio)
+    X_train, y_train = features[:split_index], target[:split_index]
+    X_val, y_val = features[split_index:], target[split_index:]
+
+    return X_train, y_train, X_val, y_val
--- prediction/auxPlot.py
+++ prediction/auxPlot.py
@@ -1,7 +1,48 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+def stacked_line_plots(df, x='time', metrics=None, title="Plot"):
+    """
+    A function to create stacked line plots for the given dataframe and metrics.
+    
+    Parameters:
+    df: DataFrame
+        The input dataframe containing the data to be plotted.
+    x: str
+        The column name to be used as the x-axis for the plots which should be present in the dataframe.
+    metrics: list
+        The list of column names to be plotted as stacked line plots.
+    title: str
+        The title for the plot.
+    """
 
+    if not metrics:
+        raise ValueError('Please provide metrics to plot')
+    # Create subplots
+    fig, axes = plt.subplots(len(metrics), 1, figsize=(20, 20), sharex=True)
+    plt.title(title)
+    # Loop through metrics and plot
+    for i, metric in enumerate(metrics):
+        axes[i].plot(df[x], df[metric], label=f'{metric} Data')
+        #axes[i].scatter(anomaly_times, pm_data[metric].iloc[original_anomalous_indexes], color='red', label='Anomaly')
+        axes[i].set_title(f'{metric}',fontsize=8)
+        axes[i].set_ylabel(metric ,fontsize=8)
+
+
+    # Label the shared x-axis
+    axes[-1].set_xlabel(x.upper())
+    ticks_to_use = df[x][::30]  # Choose every 10th time point
+    plt.xticks(ticks_to_use)
+    plt.xticks(rotation=45)
+
+    # Format the dates on x-axis
+    date_format = mdates.DateFormatter('%Y-%m-%d %H:%M')
+    plt.gca().xaxis.set_major_formatter(date_format)
+
+    fig.suptitle(title)
+    # Show plot
+    plt.show()
+    
 def stacked_lines_plots_w_anomalies(df, x_axis='datetime', metrics=None, anomaly_indices=[]):
     fig, axes = plt.subplots(len(metrics), 1, figsize=(20, 10), sharex=True)
     
